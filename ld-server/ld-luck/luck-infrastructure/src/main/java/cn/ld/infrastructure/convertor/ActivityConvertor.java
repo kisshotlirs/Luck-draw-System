2003d2fa236375e95e0a4935b71a338d15f0fc63
XiaoBai <3523915495@qq.com>
2022-12-20 19:17:06 +0800

    12.20 编写奖项、活动基本功能

--- ld-server/ld-luck/luck-infrastructure/src/main/java/cn/ld/infrastructure/convertor/ActivityConvertor.java
+++ ld-server/ld-luck/luck-infrastructure/src/main/java/cn/ld/infrastructure/convertor/ActivityConvertor.java
@@ -1,5 +1,8 @@
 package cn.ld.infrastructure.convertor;
+import cn.ld.domain.activity.ActivityTime;
+import java.time.LocalDateTime;
 
+import cn.hutool.core.util.ObjectUtil;
 import cn.ld.domain.activity.ActivityEntity;
 import cn.ld.infrastructure.database.dataObject.ActivityDB;
 
@@ -10,10 +13,33 @@ import cn.ld.infrastructure.database.dataObject.ActivityDB;
  */
 public class ActivityConvertor {
     public static ActivityDB toActivityDB(ActivityEntity entity) {
-        return null;
+        ActivityDB activityDB = new ActivityDB();
+        if (ObjectUtil.isNotNull(entity.getId())){
+            activityDB.setId(entity.getId());
+        }
+        activityDB.setActivityName(entity.getActivityName());
+        activityDB.setStartTime(entity.getActivityTime().getStartTime());
+        activityDB.setEndTime(entity.getActivityTime().getEndTime());
+        activityDB.setDescribe(entity.getDescribe());
+        activityDB.setCreateTime(entity.getCreateTime());
+        activityDB.setCreator(entity.getCreator());
+        activityDB.setUpdateTime(entity.getUpdateTime());
+        activityDB.setUpdater(entity.getUpdater());
+        return activityDB;
     }
 
     public static ActivityEntity toEntity(ActivityDB activityDB) {
-        return null;
+        ActivityEntity entity = new ActivityEntity();
+        if (ObjectUtil.isNotNull(activityDB.getId())){
+            entity.setId(activityDB.getId());
+        }
+        entity.setActivityName(activityDB.getActivityName());
+        entity.setActivityTime(new ActivityTime(activityDB.getStartTime(),activityDB.getEndTime()));
+        entity.setDescribe(activityDB.getDescribe());
+        entity.setCreateTime(activityDB.getCreateTime());
+        entity.setCreator(activityDB.getCreator());
+        entity.setUpdateTime(activityDB.getUpdateTime());
+        entity.setUpdater(activityDB.getUpdater());
+        return entity;
     }
 }
